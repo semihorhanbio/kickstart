b5d9ce11edfe9edf530bdbc8646a4e39f219d60f
Semih Orhan <sorhan3.14@gmail.com>
2023-12-10 23:05:43 +0300

    deploy contract

---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "tongue better still rival parade myth conduct taxi layer cliff arch invest",
+  "https://sepolia.infura.io/v3/3af873f73ea74757b7d57405352a2f4c"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
