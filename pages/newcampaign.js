761e32cbdf802ec15022afbac59b483d31744a2a
Semih Orhan <sorhan3.14@gmail.com>
2023-12-10 23:51:58 +0300

    add basic routing to app

---
+++ pages/newcampaign.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function NewCampaign() {
+  return (
+    <div>
+      <h1>New Campaign</h1>
+    </div>
+  );
+}
---
+++ pages/show.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Show() {
+  return (
+    <div>
+      <h1>Welcome to the show page!</h1>
+    </div>
+  );
+}
