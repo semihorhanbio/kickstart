19e6b7d0bf2a4f1e11d217cb3647d6524a903a3f
Semih Orhan <sorhan3.14@gmail.com>
2023-12-11 14:50:09 +0300

    change web3 to become used without metamask

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,16 @@
 import Web3 from "web3";
 
-window.ethereum.request({ method: "eth_requestAccounts" });
+let web3;
 
-const web3 = new Web3(window.ethereum);
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "https://sepolia.infura.io/v3/3af873f73ea74757b7d57405352a2f4c"
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
