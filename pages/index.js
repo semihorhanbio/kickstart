ce98050431e6c5acd18254161b08242452854a41
Semih Orhan <sorhan3.14@gmail.com>
2023-12-15 16:23:59 +0300

    adding css to card

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,15 @@ class CampaignsIndex extends Component {
   }
 
   render() {
-    return <div>{this.renderCampaigns()}</div>;
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+        {this.renderCampaigns()}
+      </div>
+    );
   }
 }
 
