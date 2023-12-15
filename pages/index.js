398d516b75680057bbd2bd3c5f308e165868224b
Semih Orhan <sorhan3.14@gmail.com>
2023-12-15 16:31:03 +0300

    adding button

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 
 class CampaignsIndex extends Component {
@@ -27,6 +27,7 @@ class CampaignsIndex extends Component {
           href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
         />
         {this.renderCampaigns()}
+        <Button content="Create Campaign" icon="add circle" primary />
       </div>
     );
   }
