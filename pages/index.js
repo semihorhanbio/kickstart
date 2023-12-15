017255951beab61f893535559b67da22b6c2cfdd
Semih Orhan <sorhan3.14@gmail.com>
2023-12-15 15:56:45 +0300

    use get initial props to get data

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import factory from "../ethereum/factory";
 
 class CampaignsIndex extends Component {
-  async componentDidMount() {
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
+    return { campaigns };
   }
 
   render() {
-    return <h1>campaigns</h1>;
+    return <h1>{this.props.campaigns[0]}</h1>;
   }
 }
 
