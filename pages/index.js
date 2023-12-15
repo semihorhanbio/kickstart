b72f4a85e091cedec279c4d287558153f988cf80
Semih Orhan <sorhan3.14@gmail.com>
2023-12-15 16:17:08 +0300

    rendering card

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 
 class CampaignsIndex extends Component {
@@ -7,8 +8,19 @@ class CampaignsIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  }
+
   render() {
-    return <h1>{this.props.campaigns[0]}</h1>;
+    return <div>{this.renderCampaigns()}</div>;
   }
 }
 
