31fcda8cc7a4aacb88048c05fc6005f50a937789
Semih Orhan <sorhan3.14@gmail.com>
2023-12-17 14:11:38 +0300

    make two col layout

--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,13 @@ class CampaignsIndex extends Component {
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
           />
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          />
           {this.renderCampaigns()}
-          <Button content="Create Campaign" icon="add circle" primary />
         </div>
       </Layout>
     );
