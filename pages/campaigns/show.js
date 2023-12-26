65bde1349bb9c09638fbc4d97a347d6fa540228d
Semih Orhan <sorhan3.14@gmail.com>
2023-12-26 10:17:35 +0300

    access campaign from getinitialprops

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
-    console.log(props);
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+    console.log(summary);
     return {};
   }
   render() {
