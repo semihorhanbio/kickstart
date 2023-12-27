825cf252ab2162171ea73cf78f0d163c07214413
Semih Orhan <sorhan3.14@gmail.com>
2023-12-27 15:41:05 +0300

    create request row component

---
+++ components/RequestRow.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RequestRow extends Component {
+  render() {
+    return <div>RequestRow</div>;
+  }
+}
+
+export default RequestRow;
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,6 +3,7 @@ import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
@@ -18,6 +19,18 @@ class RequestIndex extends Component {
         })
     );
     return { address, requests, requestCount };
+  }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          request={request}
+          address={this.props.address}
+        />
+      );
+    });
   }
 
   render() {
@@ -42,6 +55,7 @@ class RequestIndex extends Component {
               <HeaderCell>Finalize</HeaderCell>
             </Row>
           </Header>
+          <Body>{this.renderRows()}</Body>
         </Table>
       </Layout>
     );
