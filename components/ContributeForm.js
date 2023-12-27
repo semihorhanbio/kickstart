3ac0bd15ba3e6f3c634513ecf4c68a95c346d594
Semih Orhan <sorhan3.14@gmail.com>
2023-12-27 06:07:21 +0300

    add spinner and error handlers

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -7,12 +7,16 @@ import { Router } from "../routes";
 class ContributeForm extends Component {
   state = {
     value: "",
+    errorMessage: "",
+    loading: false,
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
 
     const campaign = Campaign(this.props.address);
+
+    this.setState({ loading: true, errorMessage: "" });
 
     try {
       const accounts = await web3.eth.getAccounts();
@@ -24,11 +28,12 @@ class ContributeForm extends Component {
     } catch (err) {
       this.setState({ errorMessage: err.message });
     }
+    this.setState({ loading: false, value: "" });
   };
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -38,7 +43,10 @@ class ContributeForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>Contribute!</Button>
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute!
+        </Button>
       </Form>
     );
   }
