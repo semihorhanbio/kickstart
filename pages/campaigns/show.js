9bcaddd800d4953eebe4993ef1da7e23322be20b
Semih Orhan <sorhan3.14@gmail.com>
2023-12-27 05:33:12 +0300

    add contribute form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card } from "semantic-ui-react";
 import Layout from "../../components/Layout";
+import ContributeForm from "../../components/ContributeForm";
 import Campaign from "../../ethereum/campaign";
 import web3 from "../../ethereum/web3";
 
@@ -71,6 +72,7 @@ class CampaignShow extends Component {
       <Layout>
         <h3>Campaign Show</h3>
         {this.renderCards()}
+        <ContributeForm />
       </Layout>
     );
   }
